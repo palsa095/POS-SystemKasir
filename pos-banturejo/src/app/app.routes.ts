46c75d661f100a8ab56e1a4f3df78d4049a3ca2e
palsa095 <alfatihberlianpalsa@gmail.com>
2026-08-09 21:25:55 +0700

    feat: create KasirComponent and configure default route to Kasir module

--- pos-banturejo/src/app/app.routes.ts
+++ pos-banturejo/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { KasirComponent } from './kasir/kasir.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'kasir', pathMatch: 'full' },
+    { path: 'kasir', component: KasirComponent }
+];
---
+++ pos-banturejo/src/app/kasir/kasir.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { KasirComponent } from './kasir.component';
+
+describe('KasirComponent', () => {
+  let component: KasirComponent;
+  let fixture: ComponentFixture<KasirComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KasirComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KasirComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
